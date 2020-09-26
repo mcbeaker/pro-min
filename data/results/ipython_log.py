10939bb21ac04624524f1b4ef8b033ce62bdaf52
Kenneth McGuinness <kenneth.mcguinness@gmail.com>
2020-09-26 18:42:48 -0400

    results and valence info

---
+++ data/results/ipython_log.py
@@ -0,0 +1,48 @@
+# IPython log file
+
+import pandas as pd
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('start', '')
+get_ipython().run_line_magic('logstart', '')
+import pandas as pd
+import os
+os.getcwd()
+os.listdir()
+df = pd.read_csv('min_pro_0_1.5_dictionaryValues.csv)
+df = pd.read_csv('min_pro_0_1.5_dictionaryValues.csv')
+df
+df[df.type='Mineral' & df.atomName='FE']
+df[df.type=='Mineral' & df.atomName=='FE']
+df[df.type=='Mineral' & (df.atomName=='FE')]
+df[(df.type=='Mineral') & (df.atomName=='FE')]
+df[(df.type=='Mineral') & (df.atomName=='FE') & (df.valence.between(1,2))]
+get_ipython().run_line_magic('hist', '')
+get_ipython().run_line_magic('logstop', '')
+get_ipython().run_line_magic('logon', '')
+get_ipython().run_line_magic('logstart', 'ipython_log.py append')
+get_ipython().run_line_magic('logstart', 'ipython_log.py append -t')
+get_ipython().run_line_magic('logoff', '')
+get_ipython().run_line_magic('logon', '')
+get_ipython().run_line_magic('logstart', 'ipython_log.py append')
+get_ipython().run_line_magic('logstart', 'ipython_log.py append -t')
+get_ipython().run_line_magic('logoff', '')
+get_ipython().run_line_magic('logstop', '')
+get_ipython().run_line_magic('lsmagic', '')
+get_ipython().run_line_magic('logstart', 'ipython_log.py -t append')
+get_ipython().run_line_magic('logstop', '')
+get_ipython().run_line_magic('logstate', '')
+get_ipython().run_line_magic('logstart', '-t ipython_log.py append')
+# Sat, 05 Sep 2020 12:21:17
+get_ipython().run_line_magic('history', '')
+# Sat, 05 Sep 2020 12:22:02
+get_ipython().run_line_magic('recall', '')
+# Sat, 05 Sep 2020 12:22:10
+Available line magics:
+get_ipython().run_line_magic('alias', ' %alias_magic  %autoawait  %autocall  %autoindent  %automagic  %bookmark  %cat  %cd  %clear  %colors  %conda  %config  %cp  %cpaste  %debug  %dhist  %dirs  %doctest_mode  %ed  %edit  %env  %gui  %hist  %history  %killbgscripts  %ldir  %less  %lf  %lk  %ll  %load  %load_ext  %loadpy  %logoff  %logon  %logstart  %logstate  %logstop  %ls  %lsmagic  %lx  %macro  %magic  %man  %matplotlib  %mkdir  %more  %mv  %notebook  %page  %paste  %pastebin  %pdb  %pdef  %pdoc  %pfile  %pinfo  %pinfo2  %pip  %popd  %pprint  %precision  %prun  %psearch  %psource  %pushd  %pwd  %pycat  %pylab  %quickref  %recall  %rehashx  %reload_ext  %rep  %rerun  %reset  %reset_selective  %rm  %rmdir  %run  %save  %sc  %set_env  %store  %sx  %system  %tb  %time  %timeit  %unalias  %unload_ext  %who  %who_ls  %whos  %xdel  %xmode')
+
+Available cell magics:
+get_ipython().run_line_magic('%!', ' %%HTML  %%SVG  %%bash  %%capture  %%debug  %%file  %%html  %%javascript  %%js  %%latex  %%markdown  %%perl  %%prun  %%pypy  %%python  %%python2  %%python3  %%ruby  %%script  %%sh  %%svg  %%sx  %%system  %%time  %%timeit  %%writefile')
+
+Automagic is ON, % prefix IS NOT needed for line magics.
+# Sat, 05 Sep 2020 12:22:36
+exit()
